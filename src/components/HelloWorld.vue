9e965d1b475682b89ccb49612077b792a0666768
miskolczi <csego.miskolczi@innonic.com>
2019-12-29 19:57:29 +0100

    first try to make landing

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,59 +1,48 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank" rel="noopener">unit-jest</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <section class="hero is-large is-background-white">
+    <div class="hero-head">
+      <div class="container">
+         <nav class="navbar">
+           <div class="navbar-brand">
+             <a class="navbar-item" href="https://bulma.io">
+               <img src="http://m-mathematics.tancode.hu/assets/images/site_logo.png" width="112" height="28">
+             </a>
+             <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+               <span aria-hidden="true"></span>
+               <span aria-hidden="true"></span>
+               <span aria-hidden="true"></span>
+             </a>
+           </div>
+           <div class="navbar-menu">
+             <div class="navbar-start">
+               <div class="navbar-item"><a>Teachers</a></div>
+               <div class="navbar-item"><a>About us</a></div>
+             </div>
+
+             <div class="navbar-end">
+               <div class="navbar-item"><a>Log in</a></div>
+               <div class="navbar-item"><a>Sign up</a></div>
+             </div>
+           </div>
+         </nav>
+      </div>
+    </div>
+    <div class="hero-body maths-banner">
+      <div class="container has-text-centered is-size-1">
+        <h1 class="title has-text-white">
+          The new way to learning math
+        </h1>
+        <div class="field has-text-centered has-text-white">Join us, this is the way</div>
+      </div>
+    </div>
+  </section>
 </template>
-
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+  .maths-banner {
+    background-image: url("../assets/jeshoots-com-5EKw8Z7CgE4-unsplash.jpg");
+    background-size: 100% auto;
+    background-repeat: no-repeat;
+  }
 </style>
